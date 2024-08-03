5da2b905bca04b2bac68e7ca1a44ae0b5bf5150b
PrestigiousPreston <prestonmaina75@gmail.com>
2024-08-03 17:01:30 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -25,7 +25,7 @@ $events = $homeController->getEvents();
     <script src="assets/scripts/color-modes.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- custon css -->
+    <!-- custom css -->
     <link rel="stylesheet" href="assets/styles/style.css">
     <link rel="stylesheet" href="assets/styles/blog.css">
     <!-- page title -->
